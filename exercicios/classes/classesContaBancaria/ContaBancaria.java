8addef52c976097e6eb2b63ac5f8a5ae67e4f0b2
ManoelCavenati <140075906+ManoelCavenati@users.noreply.github.com>
2024-03-25 11:36:14 -0300

    Classe Conta Bancaria

---
+++ exercicios/classes/classesContaBancaria/ContaBancaria.java
@@ -0,0 +1,54 @@
+package exercicios.classes.classesContaBancaria;
+
+public class ContaBancaria {
+    private int numeroDaConta;
+    private int agencia;
+    private String nomeDoTitular;
+    private double saldo;
+
+    public ContaBancaria(int numeroDaConta, int agencia, String nomeDoTitular, double saldoInicial) {
+        this.numeroDaConta = numeroDaConta;
+        this.agencia = agencia;
+        this.nomeDoTitular = nomeDoTitular;
+        this.saldo = saldoInicial;
+    }
+
+    public void depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+            System.out.println("Depósito de R$" + valor + " realizado com sucesso.");
+        } else {
+            System.out.println("O valor do depósito deve ser maior que zero.");
+        }
+    }
+
+    public void sacar(double valor) {
+        if (valor > 0) {
+            if (saldo >= valor) {
+                saldo -= valor;
+                System.out.println("Saque de R$" + valor + " realizado com sucesso.");
+            } else {
+                System.out.println("Saldo insuficiente para realizar o saque.");
+            }
+        } else {
+            System.out.println("O valor do saque deve ser maior que zero.");
+        }
+    }
+
+    public int getNumeroDaConta() {
+        return numeroDaConta;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public String getNomeDoTitular() {
+        return nomeDoTitular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+}
---
+++ exercicios/classes/classesContaBancaria/Principal.java
@@ -0,0 +1,22 @@
+package exercicios.classes.classesContaBancaria;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        ContaBancaria conta = new ContaBancaria(12345, 6789, "Fulano de Tal", 1000.0);
+
+        System.out.println("Informações da conta:");
+        System.out.println("Número da Conta: " + conta.getNumeroDaConta());
+        System.out.println("Agência: " + conta.getAgencia());
+        System.out.println("Nome do Titular: " + conta.getNomeDoTitular());
+        System.out.println("Saldo: R$" + conta.getSaldo());
+
+        conta.depositar(500.0);
+        System.out.println("Saldo após depósito: R$" + conta.getSaldo());
+
+        conta.sacar(200.0);
+        System.out.println("Saldo após saque: R$" + conta.getSaldo());
+
+        conta.sacar(2000.0);
+    }
+}
